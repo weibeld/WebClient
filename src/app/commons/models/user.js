bddd94a1a39379cf6cb2cbfe46e3fcb96dc1314c
Daniel Weibel <danielmweibel@gmail.com>
2020-11-05 14:45:54 +0100

    Add log outputs to user model functions
    These are the functions making ProtonMail HTTP API requests for
    user-related operations.

--- src/app/commons/models/user.js
+++ src/app/commons/models/user.js
@@ -2,20 +2,55 @@
 function User($http, url, srp) {
     const requestURL = url.build('users');
 
-    const create = (data, Password) => srp.verify.post({ Password }, requestURL(), data);
-    const get = (config = {}) => $http.get(requestURL(), config).then(({ data = {} } = {}) => data.User);
-    const code = (params) => $http.post(requestURL('code'), params);
-    const human = () => $http.get(requestURL('human'));
-    const verifyHuman = (params) => $http.post(requestURL('human'), params);
-    const check = () => (params) => $http.put(requestURL('check'), params);
-    const direct = () => $http.get(requestURL('direct'));
-    const lock = () => $http.put(requestURL('lock'));
-    const available = (params, config) => $http.get(requestURL('available'), params, config);
+    const create = (data, Password) => {
+        console.log('User.create()');
+        return srp.verify.post({ Password }, requestURL(), data);
+    };
+    const get = (config = {}) => {
+        console.log('User.get()');
+        return $http.get(requestURL(), config).then(({ data = {} } = {}) => data.User);
+    };
+    const code = (params) => {
+        console.log('User.code()');
+        return $http.post(requestURL('code'), params);
+    };
+    const human = () => {
+        console.log('User.human()');
+        return $http.get(requestURL('human'));
+    };
+    const verifyHuman = (params) => {
+        console.log('User.verifyHuman()');
+        return $http.post(requestURL('human'), params);
+    };
+    const check = () => {
+        console.log('User.check()');
+        return (params) => $http.put(requestURL('check'), params);
+    };
+    const direct = () => {
+        console.log('User.direct()');
+        return $http.get(requestURL('direct'));
+    };
 
-    const unlock = (credentials) => srp.auth.put(credentials, requestURL('unlock'));
-    const password = (credentials) => srp.auth.put(credentials, requestURL('password'));
-    const remove = (data) => $http.put(requestURL('delete'), data);
-
+    const lock = () => {
+        console.log('User.lock()');
+        return $http.put(requestURL('lock'));
+    };
+    const available = (params, config) => {
+        console.log('User.available()');
+        return $http.get(requestURL('available'), params, config);
+    };
+    const unlock = (credentials) => {
+        console.log('User.unlock()');
+        return srp.auth.put(credentials, requestURL('unlock'));
+    };
+    const password = (credentials) => {
+        console.log('User.password()');
+        return srp.auth.put(credentials, requestURL('password'));
+    };
+    const remove = (data) => {
+        console.log('User.remove()');
+        return $http.put(requestURL('delete'), data);
+    };
     return {
         requestURL,
         available,
